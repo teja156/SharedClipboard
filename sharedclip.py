88be121cab216e3fb958e4588f5e4c6d9611caf3
teja156 <tejaswaroop156@gmail.com>
2021-05-05 14:31:50 +0530

    #4

--- sharedclip.py
+++ sharedclip.py
@@ -7,20 +7,25 @@ import time
 
 IP = sys.argv[1].strip()
 SERVER_PORT = 9898
-
+client_connected = False
 
 # Bi-directional channel to send and receive data
 # Or a seperate port for sending and receiving
 
 def sendClipboardData(data):
 	global client
-	client.sendall(data.encode())
+	try:
+		client.sendall(data.encode())
+		print("Sent clipboard data")
+	except Exception as e:
+		print("Exception while trying to send data: \n%d\n"%e)
+		
 
 
 # Start the server and listen on a different thread
 
 def serverThread():
-	global IP, SERVER_PORT
+	global IP, SERVER_PORT, client_connected
 	try:
 		server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		server.bind(('',SERVER_PORT))
@@ -29,11 +34,17 @@ def serverThread():
 		conn, addr = server.accept()
 		with conn:
 			print("Connection received from : ",addr)
+			client_connected = True
 			while True:
 				data = conn.recv(1024)
 				print(addr[0]+": "+data.decode("utf-8"))
 				if not data:
 				    break
+
+				if data.decode("utf-8")=="^^enD^^":
+					client_connected = False
+					print("Client left the channel")
+
 
 
 	except Exception as e:
@@ -41,33 +52,54 @@ def serverThread():
 
 
 
-server_thread = threading.Thread(target=serverThread,args=(),daemon=True)
-server_thread.start()
+
+def main():
+	global IP,SERVER_PORT,client_connected
+	server_thread = threading.Thread(target=serverThread,args=(),daemon=True)
+	server_thread.start()
+
+	# Try connecting as client
+	client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	while 1:
+		try:
+			client.connect((IP,SERVER_PORT))
+			print("Connected as client.\n")
+			break
+		except Exception as e:
+			print("Couldn't connect as client, trying again in 5 secs..")
+			time.sleep(5)
 
 
 
-
-# Try connecting as client
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-while 1:
-	try:
-		client.connect((IP,SERVER_PORT))
-		print("Connected as client.\n")
-		break
-	except Exception as e:
-		print("Couldn't connect as client, trying again in 5 secs..")
-		time.sleep(5)
-
-print("Listening clipboard activity now.\n")
-while 1:
-	data = "No data in clipboard"
-	if cb.paste()=="" or cb.paste()==None:
-		data = cb.waitForPaste()
-	else:
-		data = cb.waitForNewPaste()
-
-
-	if data=="clipboard":
+	while not client_connected:
 		continue
 
-	sendClipboardData(data)
+
+	print("Listening clipboard activity now.\n")
+	while 1:
+		data = "No data in clipboard"
+		if cb.paste()=="" or cb.paste()==None:
+			data = cb.waitForPaste()
+		else:
+			data = cb.waitForNewPaste()
+
+
+		if data=="clipboard":
+			continue
+
+		if client_connected:
+			sendClipboardData(data)
+		else:
+			print("No client to send data.")
+			sys.exit(0)
+
+
+
+
+
+if __name__ == "__main__":
+	try:
+		main()
+	except KeyboardInterrupt:
+		print("Closing program..")
+		sendClipboardData("^^enD^^")
